5f1a54244e92917cccc005d8686be25961cab0ba
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-06 23:45:20 +0400

    some script to login or register user

--- server/server.js
+++ server/server.js
@@ -6,10 +6,54 @@ const jsonParser = express.json();
 require("./config/database").connect();
 const users = require("./models/users");
 
-app.post("/enter", (req,res) => {
+app.post("/login", async (req, res) => {
+    try {
+        const {login, password} = req.body;
 
-})
+        if (!login || !password) res.status(400).send("No data");
+
+        const user = await userSchema.find({login});
+
+        if (!user) res.status(404).send("User does not exist");
+
+
+
+    } catch (e) {
+        console.log(e);
+
+        return res.status(400).send("Error");
+    }
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const {login, password} = req.body;
+
+        if (!login || !password) res.status(400).send("No data");
+
+        const oldUser = await userSchema.find({login});
+
+        if (oldUser){
+            return res.status(400).send("user already exist")
+        }
+
+        const user = await new userSchema({
+            login: login,
+            password: password
+        });
+
+        return res.status(200)
+        // .send()
+
+    } catch (e) {
+        console.log(e);
+
+        return res.status(400).send("Error");
+    }
+});
+
+
 
 app.listen(5000, () => {
     console.log("Server is running")
-})
+});
