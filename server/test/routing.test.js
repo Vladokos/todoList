6aa43e3a9596f6c9842e1fd6a909198c4951e74a
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-08 23:34:51 +0400

    mocha test routing server

---
+++ server/test/routing.test.js
@@ -0,0 +1,60 @@
+const chai = require("chai");
+const expect = chai.expect;
+const should = chai.should;
+const chaiHttp = require("chai-http");
+const { it } = require("mocha");
+const server = require("../server");
+
+chai.use(chaiHttp);
+
+describe("Login requests", () => {
+  it("should return 404 ", () => {
+    chai
+      .request(server)
+      .post("/log")
+      .set("content-type", "application/json")
+      .send({
+        login: "asd",
+        password: "123",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body).to.be.a("object");
+        expect(res.body.message).to.be.equal("User does not exist");
+      });
+  });
+
+  it("should return 400 ", () => {
+    chai
+      .request(server)
+      .post("/log")
+      .set("content-type", "application/json")
+      .send({
+        login: "",
+        password: "123",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.a("object");
+        expect(res.body.message).to.be.equal("No data");
+      });
+  });
+});
+
+describe("Registration requests", () => {
+  it("should return 200 ", () => {
+    chai
+      .request(server)
+      .post("/reg")
+      .set("content-type", "application/json")
+      .send({
+        login: "asd",
+        password: "123",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body.message).to.be.equal("Success");
+      });
+  });
+});
