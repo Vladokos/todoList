8efa862d7e245b1b752fa5794ee5b96b43ce440c
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-11 22:39:14 +0400

    login.ts form login page

---
+++ client/src/login.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const login = document.getElementById("username") as HTMLInputElement;
+const password = document.getElementById("password") as HTMLInputElement;
+
+const button = document.getElementById("login") as HTMLButtonElement;
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  axios
+    .post("/log", {
+      login: login.value,
+      password: password.value,
+    })
+    .then((res) => {
+      if(res.data.message === "Success"){
+        window.location.replace("/main");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
