f101a64d7a7e958a3ad8f86b8d1ecce7fa1cbec7
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-16 23:50:02 +0400

    trynna add sort

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,8 +4,15 @@ for (let i = 0; i < tasks.length; i++) {
   tasks[i].childNodes[1].addEventListener("change", (e) => {
     const status: boolean = (e.target as HTMLInputElement).checked;
 
+    const card = e.composedPath()[1] as HTMLDivElement;
+
     if (status) {
       tasks[i].className += "completed";
+      console.log(i);
+      for (let j = i - 1; j >= 0; j--) {
+        console.log(tasks[j]);    
+      }
+
     } else {
       tasks[i].className = tasks[i].className.replace(/completed/g, "");
     }
