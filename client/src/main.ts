5037e183843308c8a9067db7f7a2aae9224289f2
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-20 23:37:02 +0400

    trynna add sort

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,16 @@
 const tasksHolder = (
   [...document.getElementsByClassName("tasks")] as Element[]
 )[0];
-const tasks = [...document.getElementsByClassName("task")] as Element[];
-const taskSorted = [...tasks] as Element[];
+// const tasks = [...document.getElementsByClassName("task")] as Element[];
+// const taskSorted = [...tasks] as Element[];
 
 ///way with one variable
-// const t: Record<string, Element> = {};
-//  [...document.getElementsByClassName("task")].map((e) => {
-//   const id: string = e.id;
+const tasks = [...document.getElementsByClassName("task")].map((e) => {
+  return e;
+});
 
-//   t[id] = e;
-// });
+console.log(tasks);
 
-// console.log(t);
 ///
 
 for (let i = 0; i < tasks.length; i++) {
@@ -20,49 +18,91 @@ for (let i = 0; i < tasks.length; i++) {
     const status: boolean = (e.target as HTMLInputElement).checked;
 
     if (status) {
-      const id: number = Number(tasks[i].id);
-      // console.log(id);
+      const maxId: string = tasks[tasks.length - 1].id;
+
+      // const id: string = (e.target as HTMLInputElement).parentNode
+
+      console.log((e.target as HTMLHtmlElement).parentElement);
       tasks[i].className += "completed";
 
+      tasks[i].id = maxId;
+
+
+      for (let j = i + 1; j < tasksHolder.children.length; j++) {
+        tasksHolder.children[j].id = (Number(tasks[j].id) - 1).toString();
+      }
+
+      tasksHolder.appendChild(tasks[i]);
+
       // console.log(tasks);
-
-      tasks[i].id = taskSorted[taskSorted.length - 1].id;
-
-      for (let j = id + 1; j < tasks.length; j++) {
-        taskSorted[j].id = (Number(taskSorted[j].id) - 1).toString();
-      }
-
-      taskSorted.sort((a, b) => {
-        if (a.id < b.id) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-
-      // console.log(taskSorted);
-
-      tasksHolder.removeChild(taskSorted[taskSorted.length - 1]);
-      tasksHolder.appendChild(taskSorted[taskSorted.length - 1]);
     } else {
-      console.log(taskSorted);
-
-      for (let j = taskSorted.length - 1; j >= 0; j--) {
-        if (!taskSorted[j].classList.contains("completed")) {
-          tasks[i].id = (Number(taskSorted[j].id) + 1).toString();
-          tasksHolder.insertBefore(tasks[i], taskSorted[j + 1]);
-
-          break;
-        }
-      }
-
-      tasks[i].className = tasks[i].className.replace(/completed/g, "");
-
-      // for (let j = taskSorted.length - 1; j >= 0; j--) {
-      //   if (taskSorted[j].classList.contains("completed")) {
-      //     taskSorted[j].id = (Number(taskSorted[j].id) + 1).toString();
+      // console.log(tasksHolder.children);
+      // for (let j = tasksHolder.children.length - 1; j >= 0; j--) {
+      //   const id: number = Number(tasksHolder.children[j].id);
+      //   if (tasksHolder.children[j].classList.contains("completed")) {
+      //     tasksHolder.children[j].id = (id + 1).toString();
+      //   } else {
+      //     tasks[i].id = (id + 1).toString();
+      //     tasksHolder.insertBefore(tasks[i], tasksHolder.children[j + 1]);
+      //     break;
       //   }
       // }
+      // tasks[i].className = tasks[i].className.replace(/completed/g, "");
     }
   });
 }
+
+// for (let i = 0; i < tasks.length; i++) {
+//   tasks[i].childNodes[1].addEventListener("change", (e) => {
+//     const status: boolean = (e.target as HTMLInputElement).checked;
+
+//     if (status) {
+//       const id: number = Number(tasks[i].id);
+
+//       tasks[i].className += "completed";
+
+//       tasks[i].id = taskSorted[taskSorted.length - 1].id;
+
+//       for (let j = id + 1; j < tasks.length; j++) {
+//         console.log(taskSorted[j]);
+//         taskSorted[j].id = (Number(taskSorted[j].id) - 1).toString();
+//       }
+
+//       taskSorted.sort((a, b) => {
+//         if (a.id < b.id) {
+//           return -1;
+//         } else {
+//           return 1;
+//         }
+//       });
+
+//       // tasksHolder.removeChild(taskSorted[taskSorted.length - 1]);
+//       tasksHolder.appendChild(taskSorted[taskSorted.length - 1]);
+
+//       // taskSorted.sort((a, b) => {
+//       //   if (a.id < b.id) {
+//       //     return -1;
+//       //   } else {
+//       //     return 1;
+//       //   }
+//       // });
+//     } else {
+//       for (let j = taskSorted.length - 1; j >= 0; j--) {
+//         if (!taskSorted[j].classList.contains("completed")) {
+//           tasks[i].id = (Number(taskSorted[j].id) + 1).toString();
+//           tasksHolder.insertBefore(tasks[i], taskSorted[j + 1]);
+
+//           break;
+//         }
+//       }
+
+//       tasks[i].className = tasks[i].className.replace(/completed/g, "");
+
+//       // for (let j = taskSorted.length - 1; j >= 0; j--) {
+//       //   if (taskSorted[j].classList.contains("completed")) {
+//       //     taskSorted[j].id = (Number(taskSorted[j].id) + 1).toString();
+//       //   }
+//       // }
+//     }
+//   });
+// }
