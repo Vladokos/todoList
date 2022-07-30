15c1f6acf6be9c820a482240754ba205c76fbb33
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-07-30 04:02:03 +0400

    changed to env

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 exports.connect = () => {
-    mongoose.connect("mongodb://localhost:27017/todoList", {
+    mongoose.connect(process.env.LINK_DATABASE, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     }).then(() => {
