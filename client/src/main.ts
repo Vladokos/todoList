4ecbabaa0c4a34d766cdc865747d47225148db2b
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-04 00:20:48 +0400

    added functions and event listener to add new cards in actions

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,25 @@
-import "./components/actionCard";
-import { addCard } from "./components/addCard"; 
-import "./components/additionalCard";
+import { requestCards } from "./components/requestCards";
+import { actionCard } from "./components/actionCard";
+import { addCard } from "./components/addCard";
+import { additionalCard } from "./components/additionalCard";
+import { clearListener } from "./components/clearListener";
+
+requestCards();
+
+const tasks = (
+  [...document.getElementsByClassName("tasks")] as HTMLDivElement[]
+)[0];
 
 addCard();
+
+tasks.addEventListener("DOMNodeInserted", () => {
+
+  // clearListener();
+  console.log('a');
+
+
+
+  actionCard();
+  additionalCard();
+
+});
