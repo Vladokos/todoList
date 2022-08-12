ce813714509d2be87cb5a8323e6906457f028b20
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-12 20:11:05 +0400

    now it can work with ts,css and images

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,14 @@
 const path = require("path");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  mode: "development",
-  entry: { reg: "./src/reg.ts", login: "./src/login.ts", main: "./src/main.ts" },
+  mode: "production",
+  entry: {
+    reg: "./src/reg.ts",
+    login: "./src/login.ts",
+    main: "./src/main.ts",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
@@ -10,6 +17,24 @@ module.exports = {
     extensions: [".webpack.js", ".web.js", ".ts", ".js"],
   },
   module: {
-    rules: [{ test: /\.ts$/, loader: "ts-loader" }],
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader" },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "img",
+        },
+      },
+    ],
   },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
