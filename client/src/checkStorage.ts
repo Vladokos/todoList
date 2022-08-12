9f66a1e2cb1a114a6cc94350d748cbea6bf60203
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-12 21:47:09 +0400

    added check to userId

--- client/src/checkStorage.ts
+++ client/src/checkStorage.ts
@@ -3,21 +3,24 @@ import axios from "axios";
 export const checkStorage = () => {
   const userId: string | null = sessionStorage.getItem("userId");
 
+  const pathLink: string = window.location.pathname;
+
   if (userId) {
     axios
       .post("/checkUser", { userId })
       .then((res) => {
-        if (
-          res.data.message === "Success" &&
-          window.location.pathname !== "/main"
-        ) {
+        if (res.data.message === "Success" && pathLink !== "/main") {
           window.location.replace("/main");
         }
       })
       .catch((error) => {
         sessionStorage.clear();
 
+        if (pathLink !== "/main") window.location.replace(pathLink);
+
         window.location.replace("/register");
       });
+  } else if (pathLink !== "/register" && pathLink !== "/login") {
+    window.location.replace("/register");
   }
 };
