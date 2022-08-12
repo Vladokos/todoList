e5ed0e795cc30f5d53d967c2d2504cc2ac78b362
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-12 20:16:49 +0400

    added function to request cards from server when user go in main page

---
+++ client/src/components/requestCards.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export const requestCards = (newListeners: Function) => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const userId: string | null = sessionStorage.getItem("userId");
+
+    if (!userId) return window.location.replace("/login");
+
+    const tasks = (
+      [...document.getElementsByClassName("tasks")] as HTMLDivElement[]
+    )[0];
+
+    axios
+      .get(`/getCards/${userId}`)
+      .then((res) => {
+        if (res.data.message === "Success") {
+          const cards: Array<any> = res.data.cards;
+
+          cards.sort((a, b) => {
+            if (a.order > b.order) {
+              return 1;
+            } else {
+              return -1;
+            }
+          });
+          for (let i = 0; i < cards.length; i++) {
+            tasks.innerHTML += `
+            <div class="${cards[i]._id} task " id=${cards[i].order}>
+             <input type="checkbox" /> 
+             <div> ${cards[i].task.slice(0, 16)} </div>
+              <div class="task__inner">
+                <div class="actions">
+                 <img src="./img/close.png" alt="close" id="close"/>
+                 <img src="./img/delete.png" alt="delete" id="delete" />
+                </div>
+                <textarea cols="30" rows="10">${cards[i].task}</textarea>
+                <button id="apply">apply</button>
+              </div> 
+              <div></div>
+            </div>`;
+          }
+
+          newListeners();
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  });
+};
